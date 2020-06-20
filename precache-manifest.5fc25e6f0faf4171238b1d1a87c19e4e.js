5933501f73395c3eb290c85462860dcf3ae21acd
ruaribe <wahawaha7@outlook.jp>
2020-06-20 11:40:05 +0900

    Updates

---
+++ precache-manifest.5fc25e6f0faf4171238b1d1a87c19e4e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fef35c5680ecdbac26b04ef07de6402",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "29d45bba2463c474067f",
+    "url": "/monsters-rolodex/static/css/main.9e284907.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "29d45bba2463c474067f",
+    "url": "/monsters-rolodex/static/js/main.7c6bb34d.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
